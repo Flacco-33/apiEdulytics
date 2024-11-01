6601069dfe6732685ef5bf2c09c6b020ff378cb6
Flacco-33 <lalo_hever@hotmail.com>
2024-10-31 23:28:02 -0600

    Renombrar la aplicación a Edulytics API y actualizar la descripción, versión y configuración de Swagger

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 #import routes
-from routes.user import user as user_router
 from routes.analysis import analysis as analysis_router
 from routes.data_match import dataMatch as data_Match_router
 from routes.analysis_teacher import analysisTeacher as analysis_teacher_router
@@ -10,10 +9,13 @@ from routes.save_data import saveData as save_data_router
 from docs import tags_metadata
 
 app = FastAPI(
-    title="FastAPI MongoDB",
-    description="This is a simple example of a FastAPI application with MongoDB.",
-    version="0.1.0",
-    openapi_tags=tags_metadata
+    title="Edulytics API",
+    description="Edulytics is an application designed to analyze students' perceptions of university teachers. It uses text and video analytics to evaluate feedback and emotions, providing a comprehensive view of teaching performance.",
+    version="0.1.1",
+    openapi_tags=tags_metadata,
+    root_path="/api",
+    swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"},
+    
 )
 
 app.add_middleware(
@@ -24,7 +26,6 @@ app.add_middleware(
     allow_headers=["*"],  # Esto permite todos los encabezados, puedes restringirlo a los necesarios
 )
 
-app.include_router(user_router)
 app.include_router(analysis_router)
 app.include_router(data_Match_router)
 app.include_router(analysis_teacher_router)
